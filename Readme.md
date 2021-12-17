11707a9cabcca978842c124f6180084025945ad0
Cassel-Evens <casselke@gmail.com>
2021-12-17 15:47:32 -0800

    add first test pushing information into an constructor

---
+++ Readme.md
@@ -0,0 +1,15 @@
+Test: "Should add Information into an Object"
+Code: 
+Expected Output: function Pizza(sauce, bread, protien, veggies) {
+  this.sauce = sauce;
+  this.bread = bread;
+  this.protien = protien;
+  this.veggies = veggies;
+  this.price = 0;
+}
+Pizza {
+sauce: 'White', 
+bread: 'Sourdough', 
+protien: 'Chicken', 
+veggies: 'Tato', 
+price: 0}
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
-// Buisness Logic
+// Business Logic
 
-function Pizza(sauce, bread, toppings) {
+function Pizza(sauce, bread, protien, veggies) {
   this.sauce = sauce;
   this.bread = bread;
   this.protien = protien;
   this.veggies = veggies;
-  this.price = price;
+  this.price = 0;
 }
 
 Pizza.prototype.getPrice = function() {
@@ -21,20 +21,20 @@ Pizza.prototype.getPrice = function() {
   } else if (this.sauce === "red") {
     this.price += 3
   } else if (this.sauce === "oil") {
-    this.price += 1.25;
-  };
-  if (this.protien === "pep") {
-    this.price += .50;
-  } else if (this.price === "sausage") {
     this.price += 1;
+  };
+  if (this.protein === "pep") {
+    this.price += 1;
+  } else if (this.price === "sausage") {
+    this.price += 2;
   } else if (this.price === "chicken") {
-    this.price += .75;
+    this.price += 2;
   };
   if (this.veggies === "pepper") {
-    this.price += .25;
+    this.price += 1;
   } else if (this.veggies === "artichoke") {
-    this.price += .50;
+    this.price += 2;
   } else if (this.veggies === "olive") {
-    this.price += .15;
+    this.price += 1;
   }
 };
