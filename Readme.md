12ebb97c6cad131d181e2c0e278d5d168a3f7809
Cassel-Evens <casselke@gmail.com>
2021-12-18 21:34:43 -0800

    add updated html, java, and second test on code adding inputs

--- Readme.md
+++ Readme.md
@@ -1,15 +1,21 @@
 Test: "Should add Information into an Object"
-Code: 
-Expected Output: function Pizza(sauce, bread, protien, veggies) {
+Code: function Pizza(sauce, bread, protien, veggies) {
   this.sauce = sauce;
   this.bread = bread;
   this.protien = protien;
   this.veggies = veggies;
   this.price = 0;
 }
+Expected Output: 
 Pizza {
 sauce: 'White', 
 bread: 'Sourdough', 
 protien: 'Chicken', 
 veggies: 'Tato', 
 price: 0}
+
+Test: "Should display inputted value of crust"
+Code: UI Logic : let crust = $("input[name=crust]:checked").val();
+Expected Output: Sourdough/Wheat/Gluten-Free;
+
+Test: "
--- js/scripts.js
+++ js/scripts.js
@@ -1,40 +1,77 @@
 // Business Logic
 
-function Pizza(sauce, bread, protien, veggies) {
+function Pizza(size, crust, sauce, protein, toppings)  {
+  this.size = size;
+  this.crust = crust; 
   this.sauce = sauce;
-  this.bread = bread;
-  this.protien = protien;
-  this.veggies = veggies;
+  this.protein = protein;
+  this.toppings = toppings;
   this.price = 0;
 }
 
 Pizza.prototype.getPrice = function() {
-  if (this.bread === "GF") {
-    this.price = 12;
-  } else if (this.bread === "Sourdough") {
-    this.price = 8;
-  } else if (this.bread === "Wheat") {
+  if (this.size === "sm") {
     this.price = 10;
+  } else if (this.size === "md") {
+    this.price = 14;
+  } else if (this.size === "lrg") {
+    this.price = 18;
   };
-  if (this.sauce === "white") {
+  if (this.crust === "Sourdough") {
+    this.price += 1;
+  } else if (this.crust === "Wheat")  {
     this.price += 2;
-  } else if (this.sauce === "red") {
-    this.price += 3
-  } else if (this.sauce === "oil") {
+  } else if (this.crust === "glutenFree") {
+    this.price += 3;
+  };
+  if (this.protein === "Chicken") {
+    this.price += 1;
+  } else if (this.protein === "Sausage") {
+    this.price += 2;
+  } else if (this.protein === "Pepperoni") {
     this.price += 1;
   };
-  if (this.protein === "pep") {
+  if (this.sauce === "Marinara") {
     this.price += 1;
-  } else if (this.price === "sausage") {
-    this.price += 2;
-  } else if (this.price === "chicken") {
-    this.price += 2;
-  };
-  if (this.veggies === "pepper") {
+  } else if (this.sace === "White") {
     this.price += 1;
-  } else if (this.veggies === "artichoke") {
-    this.price += 2;
-  } else if (this.veggies === "olive") {
+  } else if (this.sauce === "Oil") {
     this.price += 1;
   }
+
+  this.price += this.toppings.length;
+  console.log(this.toppings.length);
+  // console.log(this.price);
+  // return this.price;
+};
+
+
+// User Logic
+$(document).ready(function(){
+  $("form#pizza").submit(function(event)  {
+    event.preventDefault();
+    $("order").empty();
+    $("#pizza").toggle();
+    $("#output").show();
+    let myPizza = generateOrder();
+    $('#order').append(`<p><span>Size:</span> ${myPizza.size}</p>`).append(`<p><span>Crust:</span> ${myPizza.crust}</p>`).append(`<p><span>Sauce:</span> ${myPizza.sauce}</p>`).append(`<p><span>Protein:</span> ${myPizza.protein}</p>`).append(`<p><span>Toppings:</span> ${myPizza.toppings}</p>`).append(`<p><span>Order Total:$</span> ${myPizza.price}</p>`)
+    $("#order").text();
+    });
+  });
+
+
+function generateOrder()  {
+  let size = $("input[name=size]:checked").val();
+  let crust = $("input[name=crust]:checked").val();
+  let sauce = $("input[name=sauce]:checked").val();
+  console.log(size);
+  let protein = $("input[name=protein]:checked").val();
+  let toppings = [];
+  $("input[name=topping]:checked").each(function() {
+    toppings.push($(this).val());
+  });
+  let myPizza = new Pizza(size, crust, sauce, toppings, protein);
+  myPizza.getPrice();
+  let price = myPizza.getPrice();
+  return myPizza;
 };
